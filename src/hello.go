62228fd6b1b4aae33d6ed5f7b962953e6b78914e
luizotavio32 <luizotavioindiana@hotmail.com>
2022-04-19 20:11:28 -0300

    feat: variable attribution

--- src/hello.go
+++ src/hello.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Olá mundo, Luiz aqui")
+	name := "Luiz"
+	version := 1.1
+	fmt.Println("Hi", name)
+	fmt.Println("Program version:", version)
+
+	fmt.Println("1 - Initialize Monitoring")
+	fmt.Println("2 - Show Logs")
+	fmt.Println("0 - Exit")
+
+	var command int
+	fmt.Scan(&command)
+
+
 }
